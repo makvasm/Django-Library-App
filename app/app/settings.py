f3695ee061610a0dcb8aef4b049d296112953351
Maxim Kapralov <makvasm111@gmail.com>
2021-03-28 13:24:47 +0300

    hosts added

--- app/app/settings.py
+++ app/app/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    'makvasm.asuscomm.com'
+]
 
 
 # Application definition
